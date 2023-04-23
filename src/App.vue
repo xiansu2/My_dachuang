5e5f83b0acb9cae6361dca76902bd3e3142dfe25
xiansu2 <107790195+xiansu2@users.noreply.github.com>
2023-04-23 09:10:20 +0800

    1 file modified, 1 added

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<div id="app">
+  <button @click="count++">{{ count }}</button>
+</div>
+
+<script>
+import { createApp } from 'vue'
+
+const app = createApp({
+  data() {
+    return {
+      count: 0
+    }
+  }
+})
+
+app.mount('#app')
+
+</script>
