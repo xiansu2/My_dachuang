ed21ee592457f33cc2f1924bed1bd533ceb7f0e5
xiansu2 <107790195+xiansu2@users.noreply.github.com>
2023-04-23 10:07:05 +0800

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
-<div id="app">
-  <button @click="count++">{{ count }}</button>
-</div>
+<template>
+  <div class="countainer">
+    <!--渲染LoginForm和RegisterForm组件-->
+    <LoginForm></LoginForm>
+    <RegisterForm></RegisterForm>
+  </div>
+</template>
 
+<!--导入-->
 <script>
-import { createApp } from 'vue'
+import LoginForm from './components/LoginForm.vue'
+import RegisterForm from './components/RegisterForm.vue'
 
-const app = createApp({
-  data() {
-    return {
-      count: 0
-    }
+export default{
+  name:'LoginPage',
+  components:{  //注册组件
+    LoginForm,
+    RegisterForm
   }
-})
-
-app.mount('#app')
-
+}
 </script>
+
+<!--设置style-->
+<style scoped>
+.container{
+  background-color:dodgerblue;
+  display:flex;
+  justify-content:space-around;
+  margin-top:20px;
+}
+</style>
+
